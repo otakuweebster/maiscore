a6914aa7897e5561396e2409088b12464680bd7f
Gabriel Beltran <102841389+otakuweebster@users.noreply.github.com>
2025-10-29 14:13:21 -0600

    Setting up the actual base template

---
+++ app/maihome/page.tsx
@@ -0,0 +1,8 @@
+export default function MaiHome()
+{
+    return(
+        <div className="h-screen flex items-center justify-center">
+            <img className="object-cover" width='50%' src="https://i.redd.it/103j9edlmqfe1.jpeg"/>
+        </div>
+    )
+}
