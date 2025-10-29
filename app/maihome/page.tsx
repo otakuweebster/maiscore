05dfd9593773a121e634e84280a5641e81ecda3a
Gabriel Beltran <102841389+otakuweebster@users.noreply.github.com>
2025-10-29 15:03:26 -0600

    Yeah, changing the entire thing to astro instead

--- app/maihome/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MaiHome()
-{
-    return(
-        <div className="h-screen flex items-center justify-center">
-            <img className="object-cover" width='50%' src="https://i.redd.it/103j9edlmqfe1.jpeg"/>
-        </div>
-    )
-}
