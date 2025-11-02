4b8f12b8656fc0a71c0b06c76efdeea7ce7d378d
Gabriel Beltran <102841389+otakuweebster@users.noreply.github.com>
2025-11-02 14:15:47 -0600

    Created the profile card per se

--- app/components/OtakuUserProfile.tsx
+++ app/components/OtakuUserProfile.tsx
@@ -24,9 +24,34 @@ export default function OtakuUserProfile()
     return(
         <div>
             {showUser && (
-                <div className={'text-center'}>
-                    <Image src={data.options.icon_deka.png} alt="Next.js logo" width={100} height={100} unoptimized/>
-                    <p>{data.name}</p>
+                <div id="profile" className="w-full grid grid-cols-2 gap-2 p-10 rounded-4xl shadow-2xl/50" style={{
+                    backgroundImage: `url(${data.options.frame.webp}), url(${data.options.icon_deka.webp})`,
+                    backgroundSize: "cover, 50%",
+                    backgroundPosition: "center, left",
+                    backgroundRepeat: "no-repeat",
+                    backgroundColor: "rgba(255,255,255,0.4)",
+                    backgroundBlendMode: "lighten"
+                }}>
+
+                    <Image src={data.options.icon.webp} width={150} height={150} unoptimized/>
+                    <div className="w-full grid grid-row-2 gap-2 text-center">
+                        <div className='flex flex-col justify-center items-center rounded-4xl text-xl text-white text-shadow-lg/30' style={{
+                            backgroundImage: 'url(' + data.options.nameplate.webp + ')',
+                            backgroundSize: "100%",
+                            backgroundPosition: "center",
+                            "WebkitTextStroke": "3px black",
+                            paintOrder: "stroke fill"
+                        }}>
+                            {data.name}
+                        </div>
+                        <div className='grid grid-cols-2 gap-2 h-full text-center items-end text-3xl text-white text-shadow-lg/30' style={{
+                            "WebkitTextStroke": "3px black",
+                            paintOrder: "stroke fill"
+                        }}>
+                            <p>RATING: </p>
+                            <p>{data.rating}</p>
+                        </div>
+                    </div>
                 </div>
             )}
         </div>
--- app/layouts/MaiLandingPage.tsx
+++ app/layouts/MaiLandingPage.tsx
@@ -4,10 +4,13 @@ import Image from "next/image";
 export default function MaiLandingPage()
 {
     return(
-        <div id="main" className="w-screen h-screen flex justify-center items-center">
+        <div id="main" className="w-5/6 h-screen mx-auto flex justify-center items-center">
             <div className="flex flex-col justify-center items-center">
-                <Image src="assets/maiscore.png" alt="Next.js logo" width={894} height={324} unoptimized/>
-                <OtakuUserProfile/>
+                <Image src="assets/maiscore.png" width={894} height={324} unoptimized/>
+                <div className={'grid grid-cols-2 gap-5 w-full'}>
+                    <OtakuUserProfile/>
+                    
+                </div>
             </div>        
         </div>
     )
